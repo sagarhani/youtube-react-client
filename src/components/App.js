88b16c701bfdb896a2675ca5104f2e9877674196
sagarhani <sagarhani33@gmail.com>
2019-01-01 01:13:31 +0530

    Added CSS classes to align the UI

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ class App extends React.Component {
 
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onSearchSubmit('NASA Live');
+    }
+
     onSearchSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
-    this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = video => {
@@ -26,8 +33,16 @@ class App extends React.Component {
         return (
             <div className="ui container"> 
                 <SearchBar onFormSubmit={this.onSearchSubmit}/>
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
